9fcef79c4a836b7deee3766c3059c7d23fe9f782
Jessica Antunes <jessantunes@protonmail.com>
2021-03-01 16:29:13 -0500

    commit tree d41d55853f7eed583d6d86817610e151eee9a7d4

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+def Person
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    def name
+        @name
+    end
+
+end
+
+
+
+
